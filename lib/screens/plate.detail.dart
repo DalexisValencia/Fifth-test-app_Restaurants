01633692e1d799e1b96d22ed1cbcb60c5fa011f9
DalexisValencia <d.alexis.valencia@gmail.com>
2020-07-14 22:34:51 -0500

    Detallado del producto

--- lib/screens/plate.detail.dart
+++ lib/screens/plate.detail.dart
@@ -2,17 +2,158 @@ import 'package:fith_app__restaurant/interfaces/aditional.dart';
 import 'package:fith_app__restaurant/widgets/AditionalsExpansions.dart';
 import 'package:flutter/material.dart';
 
-class PlateDetailWrapper extends StatelessWidget {
+class PlateDetailWrapper extends StatefulWidget {
+  @override
+  _PlateDetailWrapperState createState() => _PlateDetailWrapperState();
+}
+
+class _PlateDetailWrapperState extends State<PlateDetailWrapper> {
+  bool scrollMaxReach = false;
+  ScrollController _controller;
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+    super.initState();
+  }
+
+  _scrollListener() {
+    // print(_controller.offset);
+    // if (_controller.offset == 100) {
+    // } else if (_controller.offset == 101) {}
+    // print(scrollMaxReach);
+    // // print(_controller.offset);
+    // if (_controller.offset >= _controller.position.maxScrollExtent &&
+    //     !_controller.position.outOfRange) {
+    //   setState(() {
+    //     scrollMaxReach = true;
+    //   });
+    //   // print("reach the bottom");
+    // }
+
+    // if (_controller.offset <= _controller.position.minScrollExtent &&
+    //     !_controller.position.outOfRange) {
+    //   setState(() {
+    //     scrollMaxReach = false;
+    //   });
+    //   // print("reach the top");
+    // }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      // color: Colors.amber,
+      child: Stack(
         children: <Widget>[
-          HeaderPlateDetails(),
-          GroupPlateBasicDetails(),
-          AmountProduct(),
-          Aditionals()
-          // RoundedOptionsContactWrapper()
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            // color: Colors.red,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: SingleChildScrollView(
+                      controller: _controller,
+                      child: Column(
+                        children: <Widget>[
+                          HeaderPlateDetails(),
+                          GroupPlateBasicDetails(),
+                          AmountProduct(),
+                          Aditionals(),
+                          SummaryIngredients(),
+                          SizedBox(
+                            height: 60,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: StackTopHeader()),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                  margin: EdgeInsets.all(0),
+                  padding: EdgeInsets.all(0),
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: SizedBox.expand(
+                    child: AddtoCar(),
+                  )),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class StackTopHeader extends StatefulWidget {
+  @override
+  _StackTopHeaderState createState() => _StackTopHeaderState();
+}
+
+class _StackTopHeaderState extends State<StackTopHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top + 20,
+      ),
+      padding: EdgeInsets.only(
+          left: MediaQuery.of(context).size.width * 0.07,
+          right: MediaQuery.of(context).size.width * 0.07),
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: 40,
+            height: 40,
+            child: RaisedButton(
+              elevation: 0,
+              splashColor: Theme.of(context).buttonColor,
+              onPressed: () {},
+              padding: EdgeInsets.all(0),
+              shape: CircleBorder(),
+              color: Theme.of(context).primaryColorDark.withOpacity(0.2),
+              child: Icon(
+                Icons.arrow_back,
+                color: Theme.of(context).primaryColorLight,
+              ),
+            ),
+          ),
+          Container(
+            width: 40,
+            height: 40,
+            child: RaisedButton(
+              elevation: 0,
+              splashColor: Theme.of(context).buttonColor,
+              onPressed: () {},
+              padding: EdgeInsets.all(0),
+              shape: CircleBorder(),
+              color: Theme.of(context).primaryColorDark.withOpacity(0.2),
+              child: Icon(
+                Icons.favorite,
+                color: Theme.of(context).primaryColorLight,
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -30,59 +171,11 @@ class HeaderPlateDetails extends StatelessWidget {
               image: ExactAssetImage('assets/banner/french-food.png'))),
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.40,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            top: MediaQuery.of(context).padding.top + 20,
-            left: 0,
-            child: Container(
-              padding: EdgeInsets.only(
-                  left: MediaQuery.of(context).size.width * 0.07,
-                  right: MediaQuery.of(context).size.width * 0.07),
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    width: 40,
-                    height: 40,
-                    child: RaisedButton(
-                      elevation: 0,
-                      splashColor: Theme.of(context).buttonColor,
-                      onPressed: () {},
-                      padding: EdgeInsets.all(0),
-                      shape: CircleBorder(),
-                      color:
-                          Theme.of(context).primaryColorDark.withOpacity(0.2),
-                      child: Icon(
-                        Icons.arrow_back,
-                        color: Theme.of(context).primaryColorLight,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    child: RaisedButton(
-                      elevation: 0,
-                      splashColor: Theme.of(context).buttonColor,
-                      onPressed: () {},
-                      padding: EdgeInsets.all(0),
-                      shape: CircleBorder(),
-                      color:
-                          Theme.of(context).primaryColorDark.withOpacity(0.2),
-                      child: Icon(
-                        Icons.favorite,
-                        color: Theme.of(context).primaryColorLight,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+      // child: Stack(
+      //   children: <Widget>[
+      //     // Aqui hiría el codigo de volver atras
+      //   ],
+      // ),
     );
   }
 }
@@ -218,7 +311,7 @@ class AditionalsState extends State<Aditionals> {
           top: MediaQuery.of(context).size.height * 0.005),
       width: MediaQuery.of(context).size.width,
       child: Text(
-        "Adicionales",
+        "Aditionals",
         style: Theme.of(context).textTheme.subtitle1.copyWith(
             fontWeight: FontWeight.bold,
             color: Theme.of(context).primaryColorDark),
@@ -236,6 +329,17 @@ class AditionalsState extends State<Aditionals> {
         AditionalsOptions(name: '... de tomate', isActive: false),
         AditionalsOptions(name: '... terillaki', isActive: false),
         AditionalsOptions(name: '... de guacamole', isActive: false)
+      ]),
+      Adittional(title: 'Naruto', isMulti: true, children: [
+        AditionalsOptions(name: 'Shikamaru', isActive: false),
+        AditionalsOptions(name: 'Shouji', isActive: false),
+        AditionalsOptions(name: 'Ino', isActive: false),
+        AditionalsOptions(name: 'Shikamaru', isActive: false),
+        AditionalsOptions(name: 'Shouji', isActive: false),
+        AditionalsOptions(name: 'Ino', isActive: false),
+        AditionalsOptions(name: 'Shikamaru', isActive: false),
+        AditionalsOptions(name: 'Shouji', isActive: false),
+        AditionalsOptions(name: 'Ino', isActive: false)
       ])
     ];
     return Container(
@@ -245,7 +349,6 @@ class AditionalsState extends State<Aditionals> {
         builder: (BuildContext context) {
           List<Widget> expandible = [];
           aditionals.map((item) {
-            print(item.children);
             expandible.add(AditionalExpansionPanel(
                 title: item.title,
                 isMulti: item.isMulti,
@@ -385,5 +488,130 @@ class _AmountProductState extends State<AmountProduct> {
         ],
       ),
     );
+  }
+}
+
+class SummaryIngredients extends StatefulWidget {
+  @override
+  _SummaryIngredientsState createState() => _SummaryIngredientsState();
+}
+
+class _SummaryIngredientsState extends State<SummaryIngredients> {
+  Widget _header() {
+    return Container(
+        margin:
+            EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.015),
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(
+            'Ingreddients',
+            textAlign: TextAlign.left,
+            style: Theme.of(context).textTheme.subtitle1.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColorDark),
+          ),
+        ));
+  }
+
+  Widget _ingredientsList() {
+    return Container(
+      // color: Colors.amber,
+      width: MediaQuery.of(context).size.width,
+      height: 80,
+      child: ListView.builder(
+          itemCount: 20,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return _cardIngredients();
+          }),
+    );
+  }
+
+  Widget _cardIngredients() {
+    return Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).primaryColorLight,
+            border: Border.all(
+                color: Theme.of(context).primaryColor.withOpacity(0.3)),
+            borderRadius: BorderRadius.circular(5)),
+        margin:
+            EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.010),
+        width: (MediaQuery.of(context).size.width / 4) -
+            MediaQuery.of(context).size.width * 0.028,
+        child: Card(
+          elevation: 0,
+          color: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  "435G",
+                  style: Theme.of(context).textTheme.subtitle1.copyWith(
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).primaryColorDark),
+                ),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  "TOMATTOES",
+                  style: Theme.of(context).textTheme.caption.copyWith(
+                      fontSize: 9,
+                      fontWeight: FontWeight.normal,
+                      color: Theme.of(context).primaryColorDark),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).size.height * 0.018,
+          top: MediaQuery.of(context).size.height * 0.015),
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(
+          left: MediaQuery.of(context).size.width * 0.07,
+          right: MediaQuery.of(context).size.width * 0.07),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[_header(), _ingredientsList()],
+      ),
+    );
+  }
+}
+
+class AddtoCar extends StatefulWidget {
+  @override
+  _AddtoCarState createState() => _AddtoCarState();
+}
+
+class _AddtoCarState extends State<AddtoCar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 100,
+        width: MediaQuery.of(context).size.width,
+        child: SizedBox(
+          child: RaisedButton(
+            onPressed: () {},
+            child: Text(
+              "Añadir Al Carrito",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  .copyWith(color: Theme.of(context).primaryColorLight),
+            ),
+          ),
+        ));
   }
 }
