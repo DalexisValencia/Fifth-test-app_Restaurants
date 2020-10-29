76c3cff4cd590ecd447813529e347ae648d83381
DalexisValencia <d.alexis.valencia@gmail.com>
2020-10-29 11:30:32 -0500

    Animando la pantalla entrante

--- lib/screens/seeMoreDishesInRestaurant.dart
+++ lib/screens/seeMoreDishesInRestaurant.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:fith_app__restaurant/blocs/bloc/dish/bloc/dish_bloc.dart';
 import 'package:fith_app__restaurant/blocs/bloc/restaurant/bloc/detailsrestaurant_bloc.dart';
 import 'package:fith_app__restaurant/constants/contansts.dart';
 import 'package:fith_app__restaurant/interfaces/Dishes.dart';
 import 'package:fith_app__restaurant/interfaces/Restaurants.dart';
 import 'package:fith_app__restaurant/sections/CustomHeader.dart';
+import 'package:fith_app__restaurant/widgets/AnimationContainerWrapper.dart';
 import 'package:fith_app__restaurant/widgets/quickViewCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,17 +20,22 @@ class SeeMoreDishesByRestaurant extends StatefulWidget {
 }
 
 class _SeeMoreDishesByRestaurantState extends State<SeeMoreDishesByRestaurant> {
-  Widget _header() {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 500),
-      height: defaultHeaderCustomHeight,
-      width: MediaQuery.of(context).size.width,
-      child: CustomHeader(
-        firstAction: 'goBack',
-        secondAction: 'search',
-        iconColors: Theme.of(context).primaryColorDark,
-      ),
-    );
+  bool animationState = true;
+  @override
+  initState() {
+    super.initState();
+    Timer(Duration(milliseconds: animationStartTime), () {
+      setState(() {
+        animationState = false;
+      });
+    });
+    // this.animationState = false;
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    // this.animationState = true;
   }
 
   Widget _screenTitle(name) {
@@ -46,8 +54,7 @@ class _SeeMoreDishesByRestaurantState extends State<SeeMoreDishesByRestaurant> {
     );
   }
 
-  Widget _body() {
-    print(MediaQuery.of(context).padding.top);
+  Widget _dishesList() {
     return Container(
         padding: EdgeInsets.symmetric(
             horizontal: MediaQuery.of(context).size.width * defaultPadding,
@@ -60,7 +67,6 @@ class _SeeMoreDishesByRestaurantState extends State<SeeMoreDishesByRestaurant> {
               Restaurants restaurantState = state.props[0];
               List<Dishes> completeList;
               String screenTitle = '';
-              print(widget.searchKey);
               switch (widget.searchKey) {
                 case 'lunchNow':
                   completeList = restaurantState.lunchNow;
@@ -78,11 +84,6 @@ class _SeeMoreDishesByRestaurantState extends State<SeeMoreDishesByRestaurant> {
                   _screenTitle(screenTitle),
                   Builder(
                     builder: (BuildContext context) {
-                      // List<Widget> dishes = [];
-                      // completeList.map((e) {
-                      //   print(e.name);
-                      //   dishes.add(Text(e.name));
-                      // }).toList();
                       return BlocProvider(
                           create: (BuildContext context) => DishBloc(),
                           child: Column(
@@ -98,17 +99,50 @@ class _SeeMoreDishesByRestaurantState extends State<SeeMoreDishesByRestaurant> {
         ));
   }
 
+  Widget _header() {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      height: defaultHeaderCustomHeight,
+      width: MediaQuery.of(context).size.width,
+      child: CustomHeader(
+        firstAction: 'goBack',
+        secondAction: 'search',
+        iconColors: Theme.of(context).primaryColorDark,
+      ),
+    );
+  }
+
+  Widget _seeMoreBody() {
+    return Column(
+      children: <Widget>[
+        _header(),
+        _dishesList(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColorLight,
-      body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          _header(),
-          _body(),
-        ],
-      )),
-    );
+        backgroundColor: Theme.of(context).primaryColorLight,
+        body: SafeArea(
+          //     child: CustomContainerAnimation(
+          //   animationChildren: animationState,
+          //   children: _seeMoreBody(),
+          // )
+          child: _seeMoreBody(),
+        ));
   }
 }
+
+// child: AnimatedOpacity(
+//     curve: Curves.easeInCubic,
+//     opacity: !animationState ? 0 : 1,
+//     duration: Duration(milliseconds: 500),
+//     child: Column(
+//       children: <Widget>[
+//         _header(),
+//         _body(),
+//       ],
+//     ))
+//
